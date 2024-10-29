f70e5dd28b4ff50c2ada1f17cf068eb0e6f024fb
EcclesN <Niall.Eccles@hmhco.com>
2024-10-29 23:31:44 +0000

    Use translation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import {useTranslations} from 'next-intl';
 
 export default function HomePage() {
+  const t = useTranslations('ProfilePage');
   return (
     <>
       <ColorSchemeToggle />
+      <h1>{t('title')}</h1>
     </>
   );
 }
