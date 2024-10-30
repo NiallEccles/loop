610a7d0e3ab7c257c314ae1d6385d70c6a50dbc2
EcclesN <Niall.Eccles@hmhco.com>
2024-10-30 23:16:24 +0000

    Add outline style through tailwind

--- src/components/ProfileImage/ProfileImage.tsx
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -8,7 +8,7 @@ export default function ProfileImage({name}:ProfileImageProps) {
     const config = genConfig(name)
     return (
         <div className="bg-amber-300 p-5">
-            <Avatar style={{ width: '8rem', height: '8rem', outline: '6px solid white' }} {...config} />
+            <Avatar className="outline outline-8 outline-white" style={{ width: '8rem', height: '8rem' }} {...config} />
         </div>
     )
 }
