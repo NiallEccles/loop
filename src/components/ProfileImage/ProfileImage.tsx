84fa9cb70d97f3dcf1afe9595c635a51e5216d4d
EcclesN <Niall.Eccles@hmhco.com>
2024-10-30 23:13:07 +0000

    Add outline around profile image

--- src/components/ProfileImage/ProfileImage.tsx
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -8,7 +8,7 @@ export default function ProfileImage({name}:ProfileImageProps) {
     const config = genConfig(name)
     return (
         <div className="bg-amber-300 p-5">
-            <Avatar style={{ width: '8rem', height: '8rem' }} {...config} />
+            <Avatar style={{ width: '8rem', height: '8rem', outline: '6px solid white' }} {...config} />
         </div>
     )
 }
