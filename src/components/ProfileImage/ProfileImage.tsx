7b6ce0b90bbbab5292744e0de83e8bb26fe438df
EcclesN <Niall.Eccles@hmhco.com>
2024-10-30 23:10:15 +0000

    Create ProfileImage component using Nice Avatar

---
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Avatar, { genConfig } from 'react-nice-avatar';
+
+type ProfileImageProps = {
+    name: string
+}
+export default function ProfileImage({name}:ProfileImageProps) {
+    const config = genConfig(name)
+    return (
+        <div className="bg-amber-300 p-5">
+            <Avatar style={{ width: '8rem', height: '8rem' }} {...config} />
+        </div>
+    )
+}
