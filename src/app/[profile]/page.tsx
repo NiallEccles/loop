dd5d856c002283aa80bf26f7c0d322f11d7799ec
EcclesN <Niall.Eccles@hmhco.com>
2024-11-01 21:41:44 +0000

    Use profile name component

--- src/app/[profile]/page.tsx
+++ src/app/[profile]/page.tsx
@@ -3,6 +3,7 @@ import {useTranslations} from 'next-intl';
 import { Container } from '@mantine/core';
 import {mockData} from '../../mockData';
 import ProfileImage from "@/components/ProfileImage/ProfileImage";
+import ProfileName from "@/components/ProfileName/ProfileName";
 
 export default function HomePage() {
   const t = useTranslations('ProfilePage');
@@ -11,8 +12,11 @@ export default function HomePage() {
     <>
       <ColorSchemeToggle />
         <Container size="sm">
-            <h1>{t('title')}</h1>
-            <ProfileImage name={mockData[8].first_name}/>
+            {/*<h1>{t('title')}</h1>*/}
+            <div className='flex'>
+                <ProfileImage name={mockData[8].first_name}/>
+                <ProfileName displayName={mockData[8].first_name} userName={mockData[8].user_name}/>
+            </div>
             <h2>{mockData[8].first_name}</h2>
             <h2>{mockData[8].user_name}</h2>
         </Container>
