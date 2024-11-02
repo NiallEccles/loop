335007ca8395d26466010cc06035245ff96a382f
EcclesN <Niall.Eccles@hmhco.com>
2024-11-02 13:41:38 +0000

    Adjust padding

--- src/components/ProfileName/ProfileName.tsx
+++ src/components/ProfileName/ProfileName.tsx
@@ -7,7 +7,7 @@ type ProfileNameProps = {
 }
 export default function ProfileName({displayName, userName}:ProfileNameProps) {
     return (
-        <div className='flex flex-col justify-end pl-3'>
+        <div className='flex flex-col justify-end pl-5'>
             <h3 className='font-bold text-lg text-black dark:text-white'>
                 {displayName}
             </h3>
