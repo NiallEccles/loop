a82e912c56833d84f6107a8d0e7b8185a6df3c32
EcclesN <Niall.Eccles@hmhco.com>
2024-11-02 13:21:45 +0000

    Update mock data to include bio

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,161 +1,202 @@
-export const mockData = [{
-    "id": 1,
-    "first_name": "Sidney",
-    "user_name": "skeningham0",
-    "email": "sgrollmann0@goo.gl",
-    "followers": 916,
-    "following": 2968,
-    "join_date": "1725818708000"
-}, {
-    "id": 2,
-    "first_name": "Cammi",
-    "user_name": "cocloney1",
-    "email": "cgilders1@creativecommons.org",
-    "followers": 2614,
-    "following": 232,
-    "join_date": "1726747243000"
-}, {
-    "id": 3,
-    "first_name": "Sheilakathryn",
-    "user_name": "slatham2",
-    "email": "sduffill2@homestead.com",
-    "followers": 1836,
-    "following": 1417,
-    "join_date": "1697588199000"
-}, {
-    "id": 4,
-    "first_name": "Ammamaria",
-    "user_name": "apanas3",
-    "email": "acoltman3@sitemeter.com",
-    "followers": 582,
-    "following": 310,
-    "join_date": "1696593008000"
-}, {
-    "id": 5,
-    "first_name": "Sheelagh",
-    "user_name": "solivetti4",
-    "email": "ssuermeier4@kickstarter.com",
-    "followers": 1627,
-    "following": 2215,
-    "join_date": "1722962360000"
-}, {
-    "id": 6,
-    "first_name": "Sibella",
-    "user_name": "scartmer5",
-    "email": "spollicatt5@cam.ac.uk",
-    "followers": 2307,
-    "following": 1556,
-    "join_date": "1722316323000"
-}, {
-    "id": 7,
-    "first_name": "Cher",
-    "user_name": "cbrunelli6",
-    "email": "cduddin6@themeforest.net",
-    "followers": 2755,
-    "following": 2008,
-    "join_date": "1699621328000"
-}, {
-    "id": 8,
-    "first_name": "Kristoffer",
-    "user_name": "ktassell7",
-    "email": "keasbie7@guardian.co.uk",
-    "followers": 2831,
-    "following": 1620,
-    "join_date": "1715078796000"
-}, {
-    "id": 9,
-    "first_name": "Otes",
-    "user_name": "obyers8",
-    "email": "obremmer8@gravatar.com",
-    "followers": 1483,
-    "following": 2886,
-    "join_date": "1696894637000"
-}, {
-    "id": 10,
-    "first_name": "Hynda",
-    "user_name": "hlaffoleylane9",
-    "email": "hgrisenthwaite9@loc.gov",
-    "followers": 1839,
-    "following": 891,
-    "join_date": "1714459358000"
-}, {
-    "id": 11,
-    "first_name": "Aveline",
-    "user_name": "aturfreya",
-    "email": "ahickena@merriam-webster.com",
-    "followers": 15,
-    "following": 1308,
-    "join_date": "1714849904000"
-}, {
-    "id": 12,
-    "first_name": "Kimbell",
-    "user_name": "kraultb",
-    "email": "kludmanb@github.io",
-    "followers": 2859,
-    "following": 462,
-    "join_date": "1729936391000"
-}, {
-    "id": 13,
-    "first_name": "Odo",
-    "user_name": "oseathwrightc",
-    "email": "omasonc@tamu.edu",
-    "followers": 2977,
-    "following": 642,
-    "join_date": "1701359755000"
-}, {
-    "id": 14,
-    "first_name": "Catie",
-    "user_name": "ccambdend",
-    "email": "cbashamd@china.com.cn",
-    "followers": 347,
-    "following": 550,
-    "join_date": "1699670067000"
-}, {
-    "id": 15,
-    "first_name": "Wakefield",
-    "user_name": "wwestalle",
-    "email": "wblascheke@discuz.net",
-    "followers": 1680,
-    "following": 287,
-    "join_date": "1729105781000"
-}, {
-    "id": 16,
-    "first_name": "Aridatha",
-    "user_name": "acurraof",
-    "email": "agarmonf@pbs.org",
-    "followers": 434,
-    "following": 538,
-    "join_date": "1707025167000"
-}, {
-    "id": 17,
-    "first_name": "Carlene",
-    "user_name": "cpietruszkag",
-    "email": "corsmang@theglobeandmail.com",
-    "followers": 1419,
-    "following": 1142,
-    "join_date": "1726583605000"
-}, {
-    "id": 18,
-    "first_name": "Suki",
-    "user_name": "sfacherh",
-    "email": "srobilarth@eventbrite.com",
-    "followers": 128,
-    "following": 2020,
-    "join_date": "1696304865000"
-}, {
-    "id": 19,
-    "first_name": "Bowie",
-    "user_name": "bshipperbottomi",
-    "email": "bdesaveryi@timesonline.co.uk",
-    "followers": 333,
-    "following": 2183,
-    "join_date": "1724070153000"
-}, {
-    "id": 20,
-    "first_name": "Wake",
-    "user_name": "wperej",
-    "email": "wmazilliusj@about.me",
-    "followers": 1626,
-    "following": 2479,
-    "join_date": "1717628179000"
-}]
+export const mockData = [
+    {
+        "id": 1,
+        "first_name": "Sidney",
+        "user_name": "skeningham0",
+        "email": "sgrollmann0@goo.gl",
+        "followers": 916,
+        "following": 2968,
+        "join_date": "1725818708000",
+        "bio": "Travel photographer 📸 Always chasing sunsets. Check out my photo gallery at photos.link/sidney"
+    },
+    {
+        "id": 2,
+        "first_name": "Cammi",
+        "user_name": "cocloney1",
+        "email": "cgilders1@creativecommons.org",
+        "followers": 2614,
+        "following": 232,
+        "join_date": "1726747243000",
+        "bio": "Food critic | Recipe developer | Dog mom 🐕 New cookbook coming soon!"
+    },
+    {
+        "id": 3,
+        "first_name": "Sheilakathryn",
+        "user_name": "slatham2",
+        "email": "sduffill2@homestead.com",
+        "followers": 1836,
+        "following": 1417,
+        "join_date": "1697588199000",
+        "bio": "Tech enthusiast & startup founder. Building the future at myproject.tech/demo ✨ Web3 believer"
+    },
+    {
+        "id": 4,
+        "first_name": "Ammamaria",
+        "user_name": "apanas3",
+        "email": "acoltman3@sitemeter.com",
+        "followers": 582,
+        "following": 310,
+        "join_date": "1696593008000",
+        "bio": "Yoga instructor 🧘‍♀️ Finding peace in chaos. Book a class: mindful.site/ammamaria"
+    },
+    {
+        "id": 5,
+        "first_name": "Sheelagh",
+        "user_name": "solivetti4",
+        "email": "ssuermeier4@kickstarter.com",
+        "followers": 1627,
+        "following": 2215,
+        "join_date": "1722962360000",
+        "bio": "Artist • Designer • Coffee addict ☕️ Creating magic one brush stroke at a time"
+    },
+    {
+        "id": 6,
+        "first_name": "Sibella",
+        "user_name": "scartmer5",
+        "email": "spollicatt5@cam.ac.uk",
+        "followers": 2307,
+        "following": 1556,
+        "join_date": "1722316323000",
+        "bio": "PhD candidate @Cambridge 📚 Researching marine biology. Follow my discoveries at ocean.blog/sibella"
+    },
+    {
+        "id": 7,
+        "first_name": "Cher",
+        "user_name": "cbrunelli6",
+        "email": "cduddin6@themeforest.net",
+        "followers": 2755,
+        "following": 2008,
+        "join_date": "1699621328000",
+        "bio": "Fashion blogger 👗 Living life in high heels. Style tips every Monday!"
+    },
+    {
+        "id": 8,
+        "first_name": "Kristoffer",
+        "user_name": "ktassell7",
+        "email": "keasbie7@guardian.co.uk",
+        "followers": 2831,
+        "following": 1620,
+        "join_date": "1715078796000",
+        "bio": "Professional gamer 🎮 Team @VirtualElite. Watch me stream: gamestream.tv/kris"
+    },
+    {
+        "id": 9,
+        "first_name": "Otes",
+        "user_name": "obyers8",
+        "email": "obremmer8@gravatar.com",
+        "followers": 1483,
+        "following": 2886,
+        "join_date": "1696894637000",
+        "bio": "Aspiring chef 🍳 Cooking up storms in London. Food pics that'll make you hungry"
+    },
+    {
+        "id": 10,
+        "first_name": "Hynda",
+        "user_name": "hlaffoleylane9",
+        "email": "hgrisenthwaite9@loc.gov",
+        "followers": 1839,
+        "following": 891,
+        "join_date": "1714459358000",
+        "bio": "Nature photographer 🌿 Capturing Earth's beauty one click at a time. Portfolio: nature.gallery/hynda"
+    },
+    {
+        "id": 11,
+        "first_name": "Aveline",
+        "user_name": "aturfreya",
+        "email": "ahickena@merriam-webster.com",
+        "followers": 15,
+        "following": 1308,
+        "join_date": "1714849904000",
+        "bio": "Just started my social media journey! 🌟 Learning to adult one day at a time"
+    },
+    {
+        "id": 12,
+        "first_name": "Kimbell",
+        "user_name": "kraultb",
+        "email": "kludmanb@github.io",
+        "followers": 2859,
+        "following": 462,
+        "join_date": "1729936391000",
+        "bio": "Software Engineer @TechGiants 💻 Building cool stuff. Check my projects: github.com/kimbell"
+    },
+    {
+        "id": 13,
+        "first_name": "Odo",
+        "user_name": "oseathwrightc",
+        "email": "omasonc@tamu.edu",
+        "followers": 2977,
+        "following": 642,
+        "join_date": "1701359755000",
+        "bio": "History buff 📚 Professor by day, medieval reenactor by weekend. Blog: history.now/odo"
+    },
+    {
+        "id": 14,
+        "first_name": "Catie",
+        "user_name": "ccambdend",
+        "email": "cbashamd@china.com.cn",
+        "followers": 347,
+        "following": 550,
+        "join_date": "1699670067000",
+        "bio": "Plant mom 🌱 Turning my apartment into an urban jungle. DM for plant care tips!"
+    },
+    {
+        "id": 15,
+        "first_name": "Wakefield",
+        "user_name": "wwestalle",
+        "email": "wblascheke@discuz.net",
+        "followers": 1680,
+        "following": 287,
+        "join_date": "1729105781000",
+        "bio": "Vintage car enthusiast 🚗 Restoring classics since '95. Workshop tours: classic.cars/wake"
+    },
+    {
+        "id": 16,
+        "first_name": "Aridatha",
+        "user_name": "acurraof",
+        "email": "agarmonf@pbs.org",
+        "followers": 434,
+        "following": 538,
+        "join_date": "1707025167000",
+        "bio": "Indie musician 🎸 Creating sounds in my bedroom studio. Listen here: soundcloud.com/aridatha"
+    },
+    {
+        "id": 17,
+        "first_name": "Carlene",
+        "user_name": "cpietruszkag",
+        "email": "corsmang@theglobeandmail.com",
+        "followers": 1419,
+        "following": 1142,
+        "join_date": "1726583605000",
+        "bio": "Travel writer ✈️ 30 countries and counting! Read my adventures at wanderlust.blog/carlene"
+    },
+    {
+        "id": 18,
+        "first_name": "Suki",
+        "user_name": "sfacherh",
+        "email": "srobilarth@eventbrite.com",
+        "followers": 128,
+        "following": 2020,
+        "join_date": "1696304865000",
+        "bio": "Bookworm 📚 Reading my way through life. Current read: The Midnight Library"
+    },
+    {
+        "id": 19,
+        "first_name": "Bowie",
+        "user_name": "bshipperbottomi",
+        "email": "bdesaveryi@timesonline.co.uk",
+        "followers": 333,
+        "following": 2183,
+        "join_date": "1724070153000",
+        "bio": "Street photographer 📷 Capturing urban life in black & white. Gallery: street.art/bowie"
+    },
+    {
+        "id": 20,
+        "first_name": "Wake",
+        "user_name": "wperej",
+        "email": "wmazilliusj@about.me",
+        "followers": 1626,
+        "following": 2479,
+        "join_date": "1717628179000",
+        "bio": "Coffee roaster ☕️ Small batch, big dreams. Order beans: wake.coffee/shop"
+    }
+]
