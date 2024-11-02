634c098a5023be213a4dd6228fc3b3abfd5ec428
EcclesN <Niall.Eccles@hmhco.com>
2024-11-02 13:54:29 +0000

    Accept followers and following props

--- src/components/ProfileDescription/ProfileDescription.tsx
+++ src/components/ProfileDescription/ProfileDescription.tsx
@@ -4,8 +4,10 @@ import {useFormatter} from 'next-intl';
 type ProfileDescription = {
     bio: string;
     date: string;
+    followers: number;
+    following: number;
 }
-export default function ProfileDescription({bio, date}: ProfileDescription) {
+export default function ProfileDescription({bio, date, followers, following}: ProfileDescription) {
     const format = useFormatter();
 
     const joinDate = format.dateTime(Number(date), {
@@ -16,7 +18,11 @@ export default function ProfileDescription({bio, date}: ProfileDescription) {
     return (
         <div className='flex flex-col justify-end pl-3'>
             <p>{bio}</p>
-            <p>Joined {joinDate}</p>
+            <p className='mt-3'>Joined {joinDate}</p>
+            <div className='flex flex-row mt-3'>
+                <p className='mr-3'>{following} Following</p>
+                <p>{followers} Followers</p>
+            </div>
         </div>
     )
 }
