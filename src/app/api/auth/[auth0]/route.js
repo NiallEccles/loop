13ce44feade68610308fe40ac0b4703e7ab01e2a
EcclesN <Niall.Eccles@hmhco.com>
2024-11-03 15:03:59 +0000

    Update handleAuth

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,23 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0'
+import jwt from 'jsonwebtoken'
 
-export const GET = handleAuth();
+const afterCallback = async (req, res, session) => {
+    const payload = {
+        userId: session.user.sub,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    }
+
+    session.user.accessToken = jwt.sign(payload, process.env.SUPABASE_JWT_SECRET)
+
+    return session
+}
+
+export const GET = handleAuth({
+    async callback(req, res) {
+        try {
+            await handleCallback(req, res, { afterCallback })
+        } catch (error) {
+            res.status(error.status || 500).end(error.message)
+        }
+    },
+})
